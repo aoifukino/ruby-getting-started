a0105397fdd7bf4ea614aac61b2ad14b460a5b97
aoi <fukino1115@gmail.com>
2020-09-15 06:41:57 +0000

    first commit

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -21,28 +21,26 @@ class LinebotController < ApplicationController
    
     events = client.parse_events_from(body)
    
-    events.each { |event|
-    case event
-         when Line::Bot::Event::Message
-           case event.type
-           when Line::Bot::Event::MessageType::Text
-             message = {
-               type: "text",
-               text: event.message["text"] + "!"
-             }
-             client.reply_message(event["replyToken"], message)
-           when Line::Bot::Event::MessageType::Location
-             message = {
-               type: "location",
-               title: "あなたはここにいますか？",
-               address: event.message["address"],
-               latitude: event.message["latitude"],
-               longitude: event.message["longitude"]
-             }
-             client.reply_message(event["replyToken"], message)
-           end
+        #card = rand(1..30)
+        card = 1
+        events.each { |event|
+         case event
+          when Line::Bot::Event::Message
+            case event.type
+            when Line::Bot::Event::MessageType::Text
+              if event.message['text'] =~ /クイズ/
+                  message = [
+                     {
+                        type: 'image',
+                        originalContentUrl: "https://sleepy-gorge-20285.herokuapp.com/home/ubuntu/ruby-getting-started/public/q" + card.to_s + ".jpg", 
+                        previewImageUrl: "https://sleepy-gorge-20285.herokuapp.com/home/ubuntu/ruby-getting-started/public/q" + card.to_s + ".jpg"
+                      }
+                    ]
+                 client.reply_message(event["replyToken"], message)
+              end
+           end 
          end
-       }
+        }
    
        head :ok
      end
